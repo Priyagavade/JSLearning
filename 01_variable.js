fface9bd2031b3031b5f99624afae181c0a9cb8d
Priyagavade <priyagavade5@gmail.com>
2023-09-20 08:37:27 +0530

    variableassign
    variableassign

---
+++ 01_variable.js
@@ -0,0 +1,17 @@
+
+var firstName; // variable declaration
+firstName = "Priya";  // Variable initialization
+console.log(firstName);
+firstName = "Ms.priya"; // Variable update
+console.log(firstName);
+
+
+var lastName = "gavade"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "6"; // initialization
+console.log(height);
+height = "6.2 feet";// update
+console.log(height);
---
+++ 01_variableAssign.js
@@ -0,0 +1,22 @@
+
+
+var myFullName;
+console.log(myFullName);
+ myFullName="Priya Gavade";
+console.log(myFullName);
+var myFullName="Priya Gavade";
+console.log("Full Name",myFullName);
+
+var MyLove="I love only JS"
+console.log("My love",MyLove);
+
+var firstname="Priya";
+var lastname="Gavade";
+var age="25";
+var address="Raigad";
+var pinCode="402112";
+var gender="female";
+var isMarried="NA";
+console.log(firstname,lastname,age);
+console.log(address,pinCode);
+console.log(gender,isMarried);
