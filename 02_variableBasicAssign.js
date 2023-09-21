08ed4020dd7cdd0f4b89c6cae9f6529f0824a170
Priyagavade <priyagavade5@gmail.com>
2023-09-21 13:38:33 +0530

    update2
    update2

--- 02_variableBasicAssign.js
+++ 02_variableBasicAssign.js
@@ -3,7 +3,15 @@ console.log("**************Assignment 02 Variable basic*******************");
 
 console.log("________________Ques:-->1___________________________");
 
-var name="priya Gavade";
+var name="pooja";
+console.log("name of student:-",name,",datatype of-",typeof name);
+
+
+var name="shruti";
+console.log("name of student:-",name,",datatype of-",typeof name);
+
+
+var name="priya ";
 console.log("name of student:-",name,",datatype of-",typeof name);
 
 console.log("________________________________________________________");
@@ -11,10 +19,25 @@ console.log("________________________________________________________");
 var Roll_number=45;
 console.log("Roll_number of student:-", Roll_number,",datatype of-",typeof  Roll_number);
 
+var Roll_number=25;
+console.log("Roll_number of student:-", Roll_number,",datatype of-",typeof  Roll_number);
+
+var Roll_number=55;
+console.log("Roll_number of student:-", Roll_number,",datatype of-",typeof  Roll_number);
+
+
 console.log("________________________________________________________");
 
 var Student_is_present=true;
 console.log("active:-", Student_is_present,",datatype of-",typeof Student_is_present );
+
+var Student_is_present=true;
+console.log("active:-", Student_is_present,",datatype of-",typeof Student_is_present );
+
+
+var Student_is_present=false;
+console.log("active:-", Student_is_present,",datatype of-",typeof Student_is_present );
+
 
 console.log("________________________________________________________");
 
