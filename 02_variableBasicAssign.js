305dbb193e3cd519ced1c71fd912fb3d443d0c20
Priyagavade <priyagavade5@gmail.com>
2023-09-21 09:05:21 +0530

    new assign
    new assign

---
+++ 02_variableBasicAssign.js
@@ -0,0 +1,30 @@
+
+console.log("**************Assignment 02 Variable basic*******************");
+
+console.log("________________Ques:-->1___________________________");
+
+var name="priya Gavade";
+console.log("name of student:-",name,",datatype of-",typeof name);
+
+console.log("________________________________________________________");
+
+var Roll_number=45;
+console.log("Roll_number of student:-", Roll_number,",datatype of-",typeof  Roll_number);
+
+console.log("________________________________________________________");
+
+var Student_is_present=true;
+console.log("active:-", Student_is_present,",datatype of-",typeof Student_is_present );
+
+console.log("________________________________________________________");
+
+console.log("***************NULL vs UNDEFINED******************");
+
+var x = null;
+console.log( "Value of X,",x,",type:->",typeof null);
+
+var y
+console.log("value of y:-",y,",type->",typeof y);
+
+
+
