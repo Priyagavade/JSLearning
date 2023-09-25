351548f708175eb380a7d287a52f24ee74e31a1b
Priyagavade <priyagavade5@gmail.com>
2023-09-25 19:40:57 +0530

    string
    string

---
+++ 03_functionAss02.js
@@ -0,0 +1,12 @@
+console.log("==========================================BANK DETAILS===========================================");
+function bankDetails(bankName,accountNum,location,pinCode){
+    console.log("Bank Name:",bankName,"   Account NO.:",accountNum,"    Location:",location,"    Pincode:",pinCode);
+
+}
+bankDetails("HDFC bank",5656453456,'Pune',453456);
+console.log("_____________________________________________________________________________________________");
+bankDetails("Axis Bank",8976567890,'raigad',987654);
+console.log("___________________________________________________________________________________________");
+
+bankDetails("SBI Bank",2323456765,'nashik',  876549);
+console.log("___________________________________________________________________________________________");
---
+++ 03_stringBasicAssign1.js
@@ -0,0 +1,17 @@
+console.log("---------------------------------ques1-----------------------------");
+function stringBasic(params) {
+    console.log("My dream company is Wipro");
+}
+stringBasic()
+
+console.log("-------------ques2---------------------");
+
+var myHobby1="Reading";
+var myHobby2="singing";
+var myHobby3="painting ";
+console.log("My hobbies are: ",myHobby1+", "+  myHobby2+", "+myHobby3);
+
+console.log("=============================Step-3===================================");
+
+var total=myHobby1.length+myHobby2.length+myHobby3.length;
+console.log("Sum of all characters are present hobbies= ",total);
