0153a65b6f2c9fabecef6e3e8f08e8fc0e597368
Priyagavade <priyagavade5@gmail.com>
2023-09-28 09:54:01 +0530

    update
    update

--- 05_operatorTernaryass.js
+++ 05_operatorTernaryass.js
@@ -9,16 +9,16 @@ function malemarriageelegibility(gender,age,boyname) {
     return result;
     
 }
-var result1=malemarriageelegibility("male",25,"Billgate")
+var result1=malemarriageelegibility("male",25,"bill gate")
+
 console.log(result1);
 var result=malemarriageelegibility("male",17,"stew jobs")
-
 console.log(result);
 
 console.log("                                  ");
 console.log('*******female marriage eligibility*******');
 console.log("                                  ");
-function malemarriageelegibility(gender,age,girlname) {
+function femalemarriageelegibility(gender,age,girlname) {
 
     var result=gender=='female'  && age>=18
     ? `hey ${girlname} you are eligible for marriage `
@@ -26,8 +26,8 @@ function malemarriageelegibility(gender,age,girlname) {
     return result;
     
 }
-var result1=malemarriageelegibility("female",16,"Jenifer")
+var result1=femalemarriageelegibility("female",16,"Jenifer")
 console.log(result1);
-var result=malemarriageelegibility("female",27,"malinda gates")
+var result=femalemarriageelegibility("female",27,"malinda gates")
 
 console.log(result);
